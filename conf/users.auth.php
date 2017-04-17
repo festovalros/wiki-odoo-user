ed91e088be95b8b8a87801101e89d24e3abe45fc
Samuel Pavon <festovalros@gmail.com>
2017-04-17 00:25:21 -0400

    initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,16 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+festovalros:$1$BI60isqr$wLyidMpnqs3Dvdaq0UoBc.:Samuel Pavon:festovalros@ararauna.org.ve:admin,user
+joanespinoza74:$1$RHmDaYzV$cC7Q0vNMFT.LsZDM0LhD2.:Joan Espinoza:joanespinoza74@gmail.com:user
+vijoin:$1$Xqc9xc6u$X1vPNBgj6jE9HjmE1sfbp1:Victor Inojosa:vijoin@gmail.com:user
+jessika.colina:$1$r5MHOOwn$9VSuU8DAO33aK93tteVVc/:Jessika Colina:jessika.colina@gmail.com:user
+jvergnol:$1$0PUe1tNr$Q2rbUBlzr401cSmbIRo4p/:Julien Vergnol:jvergnol@gmail.com:user
